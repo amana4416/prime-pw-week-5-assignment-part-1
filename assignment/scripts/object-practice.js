f82fe729568469a399bab62576144a6f20a5ae65
Aman Abdirazak <amanabdirizak@icloud.com>
2022-11-27 14:44:30 -0600

    worked on a couple  more problems

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -24,15 +24,15 @@ console.log('A little about me:', me);
     created above to set its value. 
   - Console.log fullName
 */
-
-
+let fullName = me.firstName + ' ' + me.lastName;
+console.log('My full names is', fullName);
 
 /* 3. Nested arrays
   - Console.log your first favorite food
   - Console.log your last favorite food 
 */
-
-
+console.log('My first favorite food is', me.favThreeFoods[0]);
+console.log('My third favorite food is', me.favThreeFoods[2]);
 
 /* 4. Change a property of an existing object.
   - Console.log your current shoe count.
